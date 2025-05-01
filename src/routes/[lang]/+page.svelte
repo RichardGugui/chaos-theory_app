8ec2579464c513c8066a282b28c7b456e15ddea1
RichardGugui <gaguirichard94@gmail.com>
2025-05-01 14:00:00 -0700

    Initial commit

---
+++ src/routes/[lang]/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { i } from '@inlang/sdk-js';
+	// import {HeadHrefLangs} from '$shared';
+
+	import { persons, games } from '$lib/shared';
+
+	import { Aflag, Countries, Footer, Header, Hero, SupportUs, OtherGames } from '$lib/widgets';
+</script>
+
+
+<svelte:head>
+    <title>Boardgame: Philosophy of liberty</title>
+    	<!-- <HeadHrefLangs /> -->
+
+</svelte:head>
+
+
+<!-- <Header /> -->
+
+<div >
+
+<Hero />
+
+<Aflag {persons} />
+
+<SupportUs />
+
+<!-- <Countries /> -->
+
+<!-- <OtherGames {games} /> -->
+</div>
+
+<Footer {games} {persons} />
+
